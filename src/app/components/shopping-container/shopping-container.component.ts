f305dd552d41c6983ca6aede2011ac49d5ee6b12
Jeffry Gonzalez <jeff@hypertheory.com>
2022-02-28 16:13:52 -0500

    fixed hard-coded urls

--- src/app/components/shopping-container/shopping-container.component.ts
+++ src/app/components/shopping-container/shopping-container.component.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ShoppingListItem } from 'src/app/models/shopping-models';
-
+import { environment } from 'src/environments/environment'; // never import any other environment, just this one.
 @Component({
   selector: 'app-shopping-container',
   templateUrl: './shopping-container.component.html',
@@ -15,13 +15,13 @@ export class ShoppingContainerComponent implements OnInit {
   constructor(private httpClient:HttpClient) { /* angular constructors are usually empty*/ }
 
   ngOnInit(): void {
-    this.httpClient.get<{ data: ShoppingListItem[] }>('http://localhost:1337/shopping-list').subscribe(response => this.list = response.data)
+    this.httpClient.get<{ data: ShoppingListItem[] }>(`${environment.shoppingApi}shopping-list`).subscribe(response => this.list = response.data)
   }
 
 
   addNewItem(description: string) {
 
-    this.httpClient.post<ShoppingListItem>('http://localhost:1337/shopping-list', { description }).subscribe(response => {
+    this.httpClient.post<ShoppingListItem>(`${environment.shoppingApi}shopping-list`, { description }).subscribe(response => {
       this.list = [response, ...this.list]
     });
   }
